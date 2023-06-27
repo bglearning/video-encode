b8f4f354071c67af7efbdad5f3640f2ec228e76b
Bijay Gurung <bijay.learning@gmail.com>
2023-06-27 21:32:58 +0200

    Switch BLIP2 model type to be feature_extractor

--- clip_video_encode/blip_utils.py
+++ clip_video_encode/blip_utils.py
@@ -3,4 +3,4 @@ from lavis.models import load_model_and_preprocess
 
 
 device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
-blip_model, blip_vis_processors, blip_text_processors = load_model_and_preprocess("blip2_image_text_matching", "pretrain", device=device)
+blip_model, blip_vis_processors, blip_text_processors = load_model_and_preprocess("blip2_feature_extractor", "pretrain", device=device)
